559c09b170d5da9059f4e0cfe6d4b74d4281007b
Yaniv Marshaly <yaniv@sketchheroes.com>
2013-07-28 08:42:24 +0300

    Initial Commit

---
+++ CCCoverFlowLayer/AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.h
+//  CCCoverFlowLayer
+//
+//  Created by Yaniv Marshaly on 7/28/13.
+//  Copyright Sketch Heroes LTD. 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+// Added only for iOS 6 support
+@interface MyNavigationController : UINavigationController <CCDirectorDelegate>
+@end
+
+@interface AppController : NSObject <UIApplicationDelegate>
+{
+	UIWindow *window_;
+	MyNavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) MyNavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ CCCoverFlowLayer/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CCCoverFlowLayer
+//
+//  Created by Yaniv Marshaly on 7/28/13.
+//  Copyright Sketch Heroes LTD. 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
